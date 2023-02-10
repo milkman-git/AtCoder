8296804d2d262380aa2863d47902edd2273c7547
TB-Muto <kazuhito.muto@takara-net.com>
2023-02-10 10:17:05 +0900

    0210 commit

---
+++ atcoder-20230210.py
@@ -0,0 +1,13 @@
+# Atcorder Beginners Selection
+# PracticeA - Welcome to AtCoder 
+try :
+    a = int(input())
+    b,c = map(int,input().split())
+    # splitでは配列が出来上がる。
+    # map でb,cに値を返却。
+    s = input()
+    print("{} {}".format(a + b + c,s))
+    # ↑がpythonの書式フォーマットである。
+except ValueError:
+    print("input is Error.")
+    
